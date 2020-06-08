af72db215eec9c7e524b43c942d0c26b0a50e938
Takács Levente <Levente.Takacs@loxon.eu>
2020-06-08 18:09:38 +0200

    requested reproduction for #2711 ticket opened in flutterfire issuetracker

--- lib/main.dart
+++ lib/main.dart
@@ -14,52 +14,6 @@ Future<void> main() async {
   final FirebaseApp app = FirebaseApp.instance;
   final Firestore firestore = Firestore(app: app);
 
-  runApp(
-      ChangeNotifierProvider<AuthService>(
-        create: (context) => AuthService(),
-        child: MyApp(firestore: firestore,))
-  );
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({this.firestore});
-
-  final Firestore firestore;
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shooting competition app',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: FutureBuilder<FirebaseUser>(
-        future: Provider.of<AuthService>(context).getUser(),
-        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // log error to console
-            if (snapshot.error != null) {
-              print("error");
-              return Text(snapshot.error.toString());
-            }
-            // redirect to the proper page
-            return snapshot.hasData ? SignUpScreen(currentUser: snapshot.data) : FancyLoginScreen();
-          } else {
-            // show loading indicator
-            return LoadingCircle();
-          }
-        },
-      ),
-    );
-  }
-}
-
-class LoadingCircle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: CircularProgressIndicator(),
-        alignment: Alignment(0.0, 0.0),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+      title: 'Firestore Example', home: MyHomePage(firestore: firestore)));
 }
